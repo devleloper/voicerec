0eb2e86180d9093c81fdc7174b2cc6e5361dd395
DevletBoltaev <devletb@icloud.com>
2024-05-25 00:03:16 +0500

    Added explaining comments

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,24 +1,29 @@
-import 'dart:io';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:record/record.dart';
-import 'package:path/path.dart' as p;
+import 'dart:io'; // Importing dart:io for file operations.
+import 'package:flutter/cupertino.dart'; // Importing Cupertino widgets.
+import 'package:flutter/material.dart'; // Importing Material widgets.
+import 'package:google_fonts/google_fonts.dart'; // Importing Google Fonts package.
+import 'package:just_audio/just_audio.dart'; // Importing Just Audio package for audio playback.
+import 'package:path_provider/path_provider.dart'; // Importing Path Provider for accessing the file system.
+import 'package:record/record.dart'; // Importing Record package for audio recording.
+import 'package:path/path.dart'
+    as p; // Importing Path package with alias 'p' for path operations.
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({super.key});
+  HomeScreen({super.key}); // Constructor for HomeScreen.
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() =>
+      _HomeScreenState(); // Creating state for HomeScreen.
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final AudioRecorder audioRecorder = AudioRecorder();
-  String? recordingPath;
-  bool isRecording = false, isPlaying = false;
-  final AudioPlayer audioPlayer = AudioPlayer();
+  final AudioRecorder audioRecorder =
+      AudioRecorder(); // Creating an instance of AudioRecorder.
+  String? recordingPath; // Variable to store the path of the recording.
+  bool isRecording = false,
+      isPlaying = false; // Variables to manage recording and playing state.
+  final AudioPlayer audioPlayer =
+      AudioPlayer(); // Creating an instance of AudioPlayer.
 
   @override
   Widget build(BuildContext context) {
@@ -30,25 +35,26 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             Text(
               'VoiceRec',
-              // Apply Poppins font to the title text
+              // Applying Poppins font to the title text
               style: GoogleFonts.poppins(
                 fontWeight: FontWeight.w600,
               ),
             ),
-            const SizedBox(width: 8),
-            const Icon(CupertinoIcons.mic_solid),
+            const SizedBox(
+                width: 8), // Adding some space between the text and the icon.
+            const Icon(CupertinoIcons.mic_solid), // Adding a microphone icon.
           ],
         ),
       ),
-      floatingActionButton: _recordingButton(),
-      body: _buildUI(),
+      floatingActionButton:
+          _recordingButton(), // Floating action button for recording.
+      body: _buildUI(), // Building the main UI of the screen.
     );
   }
 
   /// UI
 
-  // Body
-
+  // Building the main UI of the screen.
   Widget _buildUI() {
     return SizedBox(
       width: MediaQuery.sizeOf(context).width,
@@ -56,34 +62,36 @@ class _HomeScreenState extends State<HomeScreen> {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          if (recordingPath != null)
+          if (recordingPath != null) // Checking if there is a recording.
             MaterialButton(
               onPressed: () async {
                 if (audioPlayer.playing) {
-                  audioPlayer.stop();
+                  audioPlayer
+                      .stop(); // Stop playing the recording if already playing.
                   setState(() {
-                    isPlaying = false;
+                    isPlaying = false; // Update the playing state.
                   });
                 } else {
-                  await audioPlayer.setFilePath(recordingPath!);
-                  audioPlayer.play();
+                  await audioPlayer.setFilePath(
+                      recordingPath!); // Set the file path for the audio player.
+                  audioPlayer.play(); // Start playing the recording.
                   setState(() {
-                    isPlaying = true;
+                    isPlaying = true; // Update the playing state.
                   });
                 }
               },
               child: Text(isPlaying
                   ? "Stop Playing Recording"
-                  : "Start Playing Recording"),
+                  : "Start Playing Recording"), // Button text changes based on playing state.
             ),
-          if (recordingPath == null) Text('No recording found'),
+          if (recordingPath == null)
+            Text('No recording found'), // Display if no recording is found.
         ],
       ),
     );
   }
 
-  // Recording Buttom
-
+  // Widget for the recording button.
   Widget _recordingButton() {
     return FloatingActionButton(
       highlightElevation: 0,
@@ -92,33 +100,36 @@ class _HomeScreenState extends State<HomeScreen> {
       elevation: 2,
       onPressed: () async {
         if (isRecording) {
-          String? filePath = await audioRecorder.stop();
+          String? filePath = await audioRecorder.stop(); // Stop the recording.
           if (filePath != null) {
             setState(() {
-              isRecording = false;
-              recordingPath = filePath;
+              isRecording = false; // Update the recording state.
+              recordingPath = filePath; // Set the recording path.
             });
           }
         } else {
           if (await audioRecorder.hasPermission()) {
+            // Check for recording permission.
             final Directory appDocumentsDir =
-                await getApplicationDocumentsDirectory();
+                await getApplicationDocumentsDirectory(); // Get the directory to save the recording.
 
-            final String filePath =
-                p.join(appDocumentsDir.path, "recording.wav");
+            final String filePath = p.join(appDocumentsDir.path,
+                "recording.wav"); // Set the file path for the recording.
             await audioRecorder.start(
-              RecordConfig(),
+              RecordConfig(), // Start the recording with the specified configuration.
               path: filePath,
             );
             setState(() {
-              isRecording = true;
-              recordingPath = null;
+              isRecording = true; // Update the recording state.
+              recordingPath = null; // Reset the recording path.
             });
           }
         }
       },
       child: Icon(
-        isRecording ? Icons.stop : Icons.mic,
+        isRecording
+            ? Icons.stop
+            : Icons.mic, // Icon changes based on recording state.
       ),
     );
   }
